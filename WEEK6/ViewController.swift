fedcad2ac0f57f55629c33084be7c8836f5a320e
kwon <koh9037@gmail.com>
2021-11-01 09:42:35 +0900

    Initial Commit

---
+++ WEEK6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WEEK6
+//
+//  Created by mac on 2021/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
